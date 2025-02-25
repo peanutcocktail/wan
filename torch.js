c0333e3c21b3afbb3eb57056b13a9336da4e7fef
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-25 07:59:48 -0800

    update

--- torch.js
+++ torch.js
@@ -10,7 +10,8 @@ module.exports = {
         "message": [
           "uv pip install https://huggingface.co/w-e-w/torch-2.6.0-cu128.nv/resolve/main/torch-2.6.0%2Bcu128.nv-cp310-cp310-win_amd64.whl",
           "uv pip install https://huggingface.co/w-e-w/torch-2.6.0-cu128.nv/resolve/main/torchvision-0.20.0a0%2Bcu128.nv-cp310-cp310-win_amd64.whl",
-          "uv pip install https://huggingface.co/DeathDaDev/torchaudio-2.6.0-cu128/resolve/main/torchaudio-2.6.0a0%2Bc670ad8-cp310-cp310-win_amd64.whl"
+          "uv pip install https://huggingface.co/DeathDaDev/torchaudio-2.6.0-cu128/resolve/main/torchaudio-2.6.0a0%2Bc670ad8-cp310-cp310-win_amd64.whl",
+          "uv pip install https://huggingface.co/lldacing/flash-attention-windows-wheel/resolve/main/flash_attn-2.7.4%2Bcu124torch2.6.0cxx11abiFALSE-cp310-cp310-win_amd64.whl"
         ]
       },
       "next": null,
@@ -22,7 +23,9 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu121"
+        "message": [
+          "uv pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu121",
+          "uv pip install https://huggingface.co/lldacing/flash-attention-windows-wheel/resolve/main/flash_attn-2.7.4%2Bcu124torch2.6.0cxx11abiFALSE-cp310-cp310-win_amd64.whl"
       }
     },
     // windows amd
