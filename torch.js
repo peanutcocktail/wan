452313fceb33c64eb7f33638ee06548cba6593c3
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-25 08:03:09 -0800

    update

--- torch.js
+++ torch.js
@@ -26,6 +26,7 @@ module.exports = {
         "message": [
           "uv pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu121",
           "uv pip install https://huggingface.co/lldacing/flash-attention-windows-wheel/resolve/main/flash_attn-2.7.4%2Bcu124torch2.6.0cxx11abiFALSE-cp310-cp310-win_amd64.whl"
+        ]
       }
     },
     // windows amd
